4dd75a932e357647c61e6dc01b69e2ae0e5d8f2b
Kevins Bobo <kevins.bobo@gmail.com>
2016-05-17 14:04:34 +0800

    make db run

--- wsgi/myproject/shunchuang/views.py
+++ wsgi/myproject/shunchuang/views.py
@@ -1,8 +1,10 @@
 #coding:utf-8
 from django.shortcuts import render
 from django.http import HttpResponse
+from shunchuang.models import Person
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("shunchuang")
+    name = Person.objects.get(name="KevinsBobo")
+    return HttpResponse(name)

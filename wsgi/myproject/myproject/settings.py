5f10843d0d04a970533629f4d0a156cb64f25ebe
Kevins Bobo <kevins.bobo@gmail.com>
2016-05-17 17:12:45 +0800

    test static files 3

--- wsgi/myproject/myproject/settings.py
+++ wsgi/myproject/myproject/settings.py
@@ -102,6 +102,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+
 STATICFILES_DIRS = (
             os.path.join(BASE_DIR, "common_static"),
  )
